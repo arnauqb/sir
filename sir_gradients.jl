a39d1b30359ad0c5b4bea68e22f4aac30536bda4
Arnau Quera-Bofarull <arnauq@protonmail.com>
2023-07-29 16:53:39 +0100

    calculate mean

--- sir_gradients.jl
+++ sir_gradients.jl
@@ -136,10 +136,10 @@ function compute_analytical_gradient(beta, gamma, I_t, S_t, delta_t, n_timesteps
 	return stack(gradients)
 end
 
-Random.seed!(2)
+Random.seed!(3)
 
 n_timesteps = 50
-n_agents = 1000
+n_agents = 100
 initial_fraction_infected = 0.1
 beta = 0.1
 gamma = 0.05
@@ -154,48 +154,62 @@ end
 
 function run_ode_sir(p)
 	sol = SIR.run_ode(p[1], p[2], initial_fraction_infected, n_agents, n_timesteps, delta_t)
-	return stack(sol.u)
+	return stack(sol.u)[:,2:end]
 end
 
-function get_jacobian(diff_mode)
+function get_agent_jacobian(diff_mode)
 	dS_dBeta = Zygote.jacobian(x -> gradient_I_aux_beta(beta = x, diff_mode = diff_mode)[1], beta)[1]
 	dI_dBeta = Zygote.jacobian(x -> gradient_I_aux_beta(beta = x, diff_mode = diff_mode)[2], beta)[1]
 	dR_dGamma = Zygote.jacobian(x -> gradient_I_aux_beta(beta = beta, gamma = x, diff_mode = diff_mode)[3], beta)[1]
 	return stack([dS_dBeta, dI_dBeta, dR_dGamma])
 end
 
+function get_agent_jacobian_mean_and_std(diff_mode, n_samples)
+    jacobians = [get_agent_jacobian(diff_mode) for i = 1:n_samples]
+    return mean(jacobians), std(jacobians)
+end
 
 S_t, I_t, R_t = gradient_I_aux_beta(beta = beta)
 
 analytical_gradient = compute_analytical_gradient(beta, gamma, I_t, S_t, delta_t, n_timesteps)
 
+n_samples = 10
 println("Gumbel softmax...")
-@time ad_gradient_gs = get_jacobian(GS())
-println("Stochastic AD ...")
-@time ad_gradient_sad = get_jacobian(SAD())
+gs_mean, gs_std = get_agent_jacobian_mean_and_std(GS(), 10)
+sad_mean, sad_std = get_agent_jacobian_mean_and_std(SAD(), 10)
 println("ODE gradients...")
 @time ode_gradient = Zygote.jacobian(run_ode_sir, [beta, gamma])[1]
-ode_gradient = reshape(ode_gradient, (3, n_timesteps + 1, 2))
+ode_gradient = reshape(ode_gradient, (3, n_timesteps, 2))
 
 
 
 # make a plot with the analytical gradient for dS_dBeta, dI_dBeta, and dR_dGamma
 f, ax = subplots(1, 3, figsize = (15, 5))
+t_range = 1:n_timesteps
 ax[1].set_title(L"\frac{\partial S}{\partial \beta}")
 ax[2].set_title(L"\frac{\partial I}{\partial \beta}")
 ax[3].set_title(L"\frac{\partial R}{\partial \gamma}")
-ax[1].plot(analytical_gradient[1, :], label = "Analytical")
-ax[1].plot(ad_gradient_gs[:, 1], label = "GS")
-ax[1].plot(ad_gradient_sad[:, 1], label = "SAD")
-ax[1].plot(ode_gradient[1, :, 1], label = "ODE")
-ax[2].plot(analytical_gradient[2, :], label = "Analytical")
-ax[2].plot(ad_gradient_gs[:, 2], label = "GS")
-ax[2].plot(ad_gradient_sad[:, 2], label = "SAD")
-ax[2].plot(ode_gradient[2, :, 1], label = "ODE")
-ax[3].plot(analytical_gradient[6, :], label = "Analytical")
-ax[3].plot(ad_gradient_gs[:, 3], label = "GS")
-ax[3].plot(ad_gradient_sad[:, 3], label = "SAD")
-ax[3].plot(ode_gradient[3, :, 2], label = "ODE")
+
+ax[1].plot(t_range, analytical_gradient[1, :], label = "Analytical", color = "black")
+ax[1].plot(t_range, gs_mean[:, 1], label = "GS", color = "C0")
+ax[1].fill_between(t_range, gs_mean[:, 1] - gs_std[:, 1], gs_mean[:, 1] + gs_std[:, 1], alpha = 0.2, color = "C0")
+ax[1].plot(t_range, sad_mean[:, 1], label = "SAD", color = "C1")
+ax[1].fill_between(t_range, sad_mean[:, 1] - sad_std[:, 1], sad_mean[:, 1] + sad_std[:, 1], alpha = 0.2, color = "C1")
+ax[1].plot(t_range, ode_gradient[1, :, 1], label = "ODE", color = "C2")
+
+ax[2].plot(t_range, analytical_gradient[2, :], label = "Analytical", color = "black")
+ax[2].plot(t_range, ode_gradient[2, :, 1], label = "ODE", color = "C2")
+ax[2].plot(t_range, gs_mean[:, 2], label = "GS", color = "C0")
+ax[2].fill_between(t_range, gs_mean[:, 2] - gs_std[:, 2], gs_mean[:, 2] + gs_std[:, 2], alpha = 0.2, color = "C0")
+ax[2].plot(t_range, sad_mean[:, 2], label = "SAD", color = "C1")
+ax[2].fill_between(t_range, sad_mean[:, 2] - sad_std[:, 2], sad_mean[:, 2] + sad_std[:, 2], alpha = 0.2, color = "C1")
+
+ax[3].plot(t_range, analytical_gradient[6, :], label = "Analytical", color = "black")
+ax[3].plot(t_range, ode_gradient[3, :, 2], label = "ODE", color = "C2")
+ax[3].plot(t_range, gs_mean[:, 3], label = "GS", color = "C0")
+ax[3].fill_between(t_range, gs_mean[:, 3] - gs_std[:, 3], gs_mean[:, 3] + gs_std[:, 3], alpha = 0.2, color = "C0")
+ax[3].plot(t_range, sad_mean[:, 3], label = "SAD", color = "C1")
+ax[3].fill_between(t_range, sad_mean[:, 3] - sad_std[:, 3], sad_mean[:, 3] + sad_std[:, 3], alpha = 0.2, color = "C1")
 for i in 1:3
 	ax[i].set_xlabel("Time-step")
     ax[i].set_ylabel("Gradient")
